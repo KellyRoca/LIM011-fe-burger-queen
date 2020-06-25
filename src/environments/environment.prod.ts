210f653516b692eea885eb2d5101139dcf10b047
KellyRoca <rocarondinel.kelly@gmail.com>
2020-06-25 15:21:16 -0500

    new prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBC_1AxK8nYPiuG23wHPfk4Z7nATmbx5wY',
+    authDomain: 'burgerqueen-ee96d.firebaseapp.com',
+    databaseURL: 'https://burgerqueen-ee96d.firebaseio.com/',
+    projectId: 'burgerqueen-ee96d',
+    storageBucket: 'burgerqueen-ee96d.appspot.com',
+    messagingSenderId: '425258891708',
+  },
 };
